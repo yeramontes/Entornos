af4e723ad1c9caa67df7792e40db8b5de98d3e36
Yeram <Yeram@Pc.mshome.net>
2022-03-29 17:42:00 +0200

    Primer test clase Pruebas

--- src/generadorIp/Pruebas.java
+++ src/generadorIp/Pruebas.java
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test;
 class Pruebas {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGenerarNumero() {
+		for(int i = 0; i < 1000; i++) {
+			int numero = GeneradorIp.generarNumero();
+			assertTrue(numero >= 0 && numero <= 254);
+		}
 	}
 
+	@Test
+	void testGeneraIp() {
+		String ip = GeneradorIp.generarIp();
+		String[] numerosString = ip.split(".");
+		int[] numeros = new int[numerosString.length];
+		for(int i = 0; i < numerosString.length; i++) {
+			numeros[i] = Integer.parseInt(numerosString[i]);
+		}
+	}
 }
