cf051b1ed9072e9cee16af300116950ff6ead118
Yeram <Yeram@Pc.mshome.net>
2022-03-31 20:13:20 +0200

    Añadido test Producto

---
+++ src/factura/ProductoTest.java
@@ -0,0 +1,22 @@
+package factura;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductoTest {
+
+	@Test
+	void testNuevo() {
+		Producto p = new Producto("pan", 0.50f, 1);
+		assertTrue(p.getNombre().equals("pan"));
+		assertEquals(0.50, p.getPrecio());
+		assertEquals(1, p.getCantidad());
+	}
+
+	@Test
+	void testPrecioTotal() {
+		Producto p = new Producto("pan", 1.50f, 10);
+		assertEquals(15, p.precioTotal());
+	}
+}
